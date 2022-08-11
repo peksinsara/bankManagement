c90c56b868f1e54ae60327d1b62ec5700f7a64a8
peksinsara <peksin.sara@gmail.com>
2022-08-11 14:09:57 +0200

    additional CRUD operations

---
+++ db/query/entry.sql
@@ -0,0 +1,18 @@
+-- name: CreateEntry :one
+INSERT INTO enteries (
+    account_id,
+    amount
+) VALUES (
+             $1, 200
+         ) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM enteries
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM enteries
+WHERE account_id = $1
+ORDER BY id
+    LIMIT 2
+OFFSET 3;
