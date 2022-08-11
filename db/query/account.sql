f9d2462a9d7f42d5c9bc9b127b15289d45bbd773
peksinsara <peksin.sara@gmail.com>
2022-08-11 14:01:44 +0200

    CRUD operations

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,3 +1,4 @@
+-- name: CreateAccount :one
 INSERT INTO
     accounts (
     owner,
@@ -6,3 +7,26 @@ INSERT INTO
 )
 VALUES
     ('Sara', 100, '$') RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = 1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+WHERE owner = 'Sara'
+ORDER BY id
+    LIMIT 2
+OFFSET 3;
+
+/*couldnt run :exec*/
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = 999
+WHERE owner = 'Sara'
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE owner = 'Sara';
+
